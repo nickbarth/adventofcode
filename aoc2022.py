7f80f2deb930a1c152d688d6b87e26e6a56d1515
Nick Barth <nickbarth@live.com>
2022-12-11 14:01:41 -0700

    Update aoc2022.py

--- aoc2022.py
+++ aoc2022.py
@@ -824,6 +824,7 @@ class Day11MonkeyInTheMiddle(object):
             yield None # type: ignore
 
         lines = readlines(text)
+
         while (line := next(lines)) != None:
             if line.startswith("Monkey"):
                 monkey = Monkey()
@@ -881,30 +882,30 @@ class Day11MonkeyInTheMiddle(object):
 
 if __name__ == "__main__":
     print("[ Day 11 ]:")
-    input11 = "Monkey 0:\n" +\
-           "  Starting items: 79, 98\n" +\
-           "  Operation: new = old * 19\n" +\
-           "  Test: divisible by 23\n" +\
-           "    If true: throw to monkey 2\n" +\
-           "    If false: throw to monkey 3\n\n" +\
-           "Monkey 1:\n" +\
-           "  Starting items: 54, 65, 75, 74\n" +\
-           "  Operation: new = old + 6\n" +\
-           "  Test: divisible by 19\n" +\
-           "    If true: throw to monkey 2\n" +\
-           "    If false: throw to monkey 0\n\n" +\
-           "Monkey 2:\n" +\
-           "  Starting items: 79, 60, 97\n" +\
-           "  Operation: new = old * old\n" +\
-           "  Test: divisible by 13\n" +\
-           "    If true: throw to monkey 1\n" +\
-           "    If false: throw to monkey 3\n\n" +\
-           "Monkey 3:\n" +\
-           "  Starting items: 74\n" +\
-           "  Operation: new = old + 3\n" +\
-           "  Test: divisible by 17\n" +\
-           "    If true: throw to monkey 0\n" +\
-           "    If false: throw to monkey 1\n\n"
+    input11:str = "Monkey 0:\n" +\
+                  "  Starting items: 79, 98\n" +\
+                  "  Operation: new = old * 19\n" +\
+                  "  Test: divisible by 23\n" +\
+                  "    If true: throw to monkey 2\n" +\
+                  "    If false: throw to monkey 3\n\n" +\
+                  "Monkey 1:\n" +\
+                  "  Starting items: 54, 65, 75, 74\n" +\
+                  "  Operation: new = old + 6\n" +\
+                  "  Test: divisible by 19\n" +\
+                  "    If true: throw to monkey 2\n" +\
+                  "    If false: throw to monkey 0\n\n" +\
+                  "Monkey 2:\n" +\
+                  "  Starting items: 79, 60, 97\n" +\
+                  "  Operation: new = old * old\n" +\
+                  "  Test: divisible by 13\n" +\
+                  "    If true: throw to monkey 1\n" +\
+                  "    If false: throw to monkey 3\n\n" +\
+                  "Monkey 3:\n" +\
+                  "  Starting items: 74\n" +\
+                  "  Operation: new = old + 3\n" +\
+                  "  Test: divisible by 17\n" +\
+                  "    If true: throw to monkey 0\n" +\
+                  "    If false: throw to monkey 1\n\n"
     solution11 = Day11MonkeyInTheMiddle(input11)
     assert solution11.part1() == 10605, "❌ Part 1"; print("✅ Part 1")
     assert solution11.part2() == 2713310158, "❌ Part 2"; print("✅ Part 2\n")
