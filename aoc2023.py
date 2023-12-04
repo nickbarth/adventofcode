eb29dc8032911bd1dc210135cf3e3c68d3fdb246
Nick Barth <nickbarth@live.com>
2023-12-03 23:08:19 -0700

    Update aoc2023.py

--- aoc2023.py
+++ aoc2023.py
@@ -195,3 +195,45 @@ def Day3Part2(data):
             valid_numbers[valid].append(number)
     return sum(int(x[0]) * int(x[1]) for x in valid_numbers.values() if len(x) == 2)
 assert Day3Part2(data) == 467835, "❌"; print(" ⭐\n")
+
+print("🎄 Day 4:", end="")
+
+data = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+
+def Day4Part1(data):
+    lines = data.splitlines()
+    total = 0
+    for line in lines:
+        lotto = line.split(":")[1].strip()
+        winners, picks = lotto.split("|")
+        winners = list(map(int, winners.split()))
+        picks = list(map(int, picks.split()))
+        winning_picks = set(winners) & set(picks)
+        if len(winning_picks) == 0:
+            continue
+        total += 2**(len(winning_picks)-1)
+    return total
+assert Day4Part1(data) == 13, "❌"; print(" ⭐", end="")
+
+def Day4Part2(data):
+    lines = data.splitlines()
+    count = defaultdict(int)
+    for index, line in enumerate(lines):
+        count[index] += 1
+        lotto = line.split(":")[1].strip()
+        winners, picks = lotto.split("|")
+        winners = list(map(int, winners.split()))
+        picks = list(map(int, picks.split()))
+        winning_picks = set(winners) & set(picks)
+        if len(winning_picks) == 0:
+            continue
+        for i in range(index+1, index + len(winning_picks) + 1):
+            count[i] += count[index]
+    return sum(count.values())
+assert Day4Part2(data) == 30, "❌"; print(" ⭐\n")
